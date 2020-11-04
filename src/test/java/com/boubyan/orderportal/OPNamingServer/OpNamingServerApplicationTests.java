74455bb36cd1dfe293b5fef3456b0543b0be2ddf
Abdulrahman Saad AlShammari U <03856u@corp.BBYNUAT.com>
2020-11-04 12:06:08 +0300

    first comit

---
+++ src/test/java/com/boubyan/orderportal/OPNamingServer/OpNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boubyan.orderportal.OPNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
